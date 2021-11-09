ee534103fec906d94b7fdff32c53d5f6ac64093f
stndv <stndvbakyt@gmail.com>
2021-11-09 13:22:07 +0600

    input form

--- index.php
+++ index.php
@@ -1,45 +1,15 @@
-<?php 
-	include('function.php');
-?>
+<?php include('3-view.html'); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>MVC Task</title>
+
 </head>
 <body>
-	<div class="header">
-		<h2>Home Page</h2>
-	</div>
-	<div class="content">
-		<!-- notification message -->
-		<?php if (isset($_SESSION['success'])) : ?>
-			<div class="error success" >
-				<h3>
-					<?php 
-						echo $_SESSION['success']; 
-						unset($_SESSION['success']);
-					?>
-				</h3>
-			</div>
-		<?php endif ?>
-		<!-- logged in user information -->
-		<div class="profile_info">
-			<img src="images/user_profile.png"  >
-
-			<div>
-				<?php  if (isset($_SESSION['user'])) : ?>
-					<strong><?php echo $_SESSION['user']['username']; ?></strong>
-
-					<small>
-						<i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
-						<br>
-						<a href="index.php?logout='1'" style="color: red;">logout</a>
-					</small>
-
-				<?php endif ?>
-			</div>
-		</div>
-	</div>
+	<form method='get' action="">
+		Name: <input type="text" name="user_name" placeholder="Enter Your Name"> <br>
+		Email: <input type="email" name="user_email" placeholder="Enter Your Email"> <br>
+		Task: <textarea name="user_task" id="" cols="30" rows="10"></textarea> <br>
+		<input type="submit" value="Add">
+	</form>	
 </body>
-</html>
